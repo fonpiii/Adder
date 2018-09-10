4b0cf6dec4a0af5289454e58b6f95e35459f76ab
Pirunan Sariyang <60050222@kmitl.ac.th>
2018-09-10 19:18:03 +0700

    edit cal

---
+++ Adder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Adder{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+           System.out.println("Enter numer :"); double x = sc.nextDouble();
+          System.out.println("Enter numer :");  double y = sc.nextDouble();
+            double A;
+          A = x+y;
+            System.out.println("Ans : "+A);
+
+        
+    }
+}
